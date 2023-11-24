dd8c869b136ceb954a9cefe464d0524dc35a98d1
Tomasz Kaliński <tomasz@infosun.pl>
2023-11-24 16:49:32 +0100

    frontpage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      "./assets/**/*.js",
-      "./templates/**/*.html.twig",
-      "./templates/**/**/*.html.twig"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    mode: process.env.NODE_ENV ? 'jit' : undefined,
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/**/*.html.twig",
+        "./src/Form/**/*.php",
+        "./node_modules/tw-elements/dist/js/**/*.js"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'mint': '#7da29e'
+            },
+        },
+    },
+    plugins: [
+        require("tw-elements/dist/plugin.cjs"),
+    ],
 }
