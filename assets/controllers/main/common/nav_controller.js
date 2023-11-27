109a0239f2ec3df86217504d7a63170ac4965520
Tomasz Kaliński <tomkalon@gmail.com>
2023-11-27 22:47:39 +0100

    frontpage fixes

--- assets/controllers/main/common/nav_controller.js
+++ assets/controllers/main/common/nav_controller.js
@@ -1,12 +1,26 @@
 import { Controller } from '@hotwired/stimulus';
-import Api from '../../../bundles/Api.js'
 
 export default class extends Controller {
     static targets = [ "toggleTheme" ]
 
     connect() {
-        this.toggleThemeTarget.addEventListener('click', (event) => {
-            Api.put('main_api_common_toggle_theme');
+        const body = document.querySelector('body');
+        const toggleModeButton = this.toggleThemeTarget;
+
+        toggleModeButton.addEventListener('click', (event) => {
+            body.classList.toggle('dark');
+
+            let className;
+            if (body.classList.contains('dark')) {
+                className = 'dark';
+                toggleModeButton.classList.remove('fa-toggle-on');
+                toggleModeButton.classList.add('fa-toggle-off');
+            } else {
+                className = undefined;
+                toggleModeButton.classList.remove('fa-toggle-off')
+                toggleModeButton.classList.add('fa-toggle-on')
+            }
+            localStorage.setItem('themeMode', className);
         })
     }
 
