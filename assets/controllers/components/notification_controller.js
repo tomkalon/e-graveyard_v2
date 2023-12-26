ad093a943f98025ca6d49be81ff9009d5a574802
Tomasz Kaliński <tomkalon@gmail.com>
2023-12-26 20:50:17 +0100

    notifications

---
+++ assets/controllers/components/notification_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from '@hotwired/stimulus';
+import $ from 'jquery';
+
+export default class extends Controller {
+    static targets = [ "closeBtn" ]
+
+    connect() {
+        const notification = this.element;
+        const closeBtn = this.closeBtnTarget;
+
+        // close notification handler
+        closeNotification(closeBtn, notification);
+
+        setTimeout(() => {
+            $(notification).fadeOut(1000);
+        }, 10000);
+    }
+}
+
+function closeNotification (closeBtn, notification) {
+    $(closeBtn).click((event) => {
+        $(notification).fadeOut(500);
+    })
+}
