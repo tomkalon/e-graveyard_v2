cc01886b41bd3bd1931cb6c2b790f3a21e2c411b
Tomasz Kaliński <tomkalon@gmail.com>
2024-01-05 20:20:51 +0100

    rest

--- src/Admin/UI/Api/Controller/Grave/GraveController.php
+++ src/Admin/UI/Api/Controller/Grave/GraveController.php
@@ -5,25 +5,25 @@ namespace App\Admin\UI\Api\Controller\Grave;
 use App\Admin\Application\Command\Grave\RemoveGraveCommand;
 use App\Admin\Infrastructure\Query\Grave\GetGraveInterface;
 use App\Core\Application\CQRS\Command\CommandBusInterface;
-use FOS\RestBundle\Controller\AbstractFOSRestController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-class GraveController extends AbstractFOSRestController
+class GraveController extends AbstractController
 {
     public function get(
         string $id,
         GetGraveInterface $query
-    ): Response
+    ): JsonResponse
     {
         $data = $query->execute($id);
-        $view = $this->view($data, 200);
-        return $this->handleView($view);
+        return new JsonResponse($data->jsonSerialize());
     }
 
     public function remove(
         string $id,
         CommandBusInterface $commandBus
-    ): void {
+    ): JsonResponse {
         $commandBus->dispatch(new RemoveGraveCommand($id));
+        return new JsonResponse(true);
     }
 }
