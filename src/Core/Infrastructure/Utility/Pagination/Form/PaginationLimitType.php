8c5b8cf6928c41b5658d524cc29ca12c4561d28d
Tomasz Kaliński <tomkalon@gmail.com>
2024-01-09 19:22:35 +0100

    pagination

--- src/Core/Infrastructure/Utility/Pagination/Form/PaginationLimitType.php
+++ src/Core/Infrastructure/Utility/Pagination/Form/PaginationLimitType.php
@@ -5,7 +5,6 @@ namespace App\Core\Infrastructure\Utility\Pagination\Form;
 use App\Core\Domain\Enum\PaginationLimitEnum;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
