b178cb85394d63af5285dc3fb9025801b2b70b2d
Tomasz Kaliński <tomkalon@gmail.com>
2024-01-10 16:34:26 +0100

    add grave redirection

---
+++ src/Core/Domain/Event/PostPersistListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Core\Domain\Event;
+
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class PostPersistListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            Events::postPersist => 'postPersist'
+        ];
+    }
+
+    public function postPersist(LifecycleEventArgs $args)
+    {
+    }
+}
