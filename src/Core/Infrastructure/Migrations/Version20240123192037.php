8db2757f7c466f1679f842d49e5579b49e006e54
Tomasz Kaliński <tomkalon@gmail.com>
2024-01-23 20:30:44 +0100

    migrations

--- src/Core/Infrastructure/Migrations/Version20240123192037.php
+++ src/Core/Infrastructure/Migrations/Version20240123192037.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240122152705 extends AbstractMigration
+final class Version20240123192037 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,24 +20,28 @@ final class Version20240122152705 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE file_grave (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', grave_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', INDEX IDX_8B2D6ED0E439654A (grave_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE file_grave ADD CONSTRAINT FK_8B2D6ED0E439654A FOREIGN KEY (grave_id) REFERENCES grave (id) ON DELETE SET NULL');
-        $this->addSql('ALTER TABLE file_grave ADD CONSTRAINT FK_8B2D6ED0BF396750 FOREIGN KEY (id) REFERENCES file (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE file DROP FOREIGN KEY FK_8C9F3610E439654A');
+        $this->addSql('ALTER TABLE file DROP FOREIGN KEY FK_8C9F3610B0C855CA');
+        $this->addSql('DROP INDEX IDX_8C9F3610B0C855CA ON file');
+        $this->addSql('DROP INDEX IDX_8C9F3610E439654A ON file');
         $this->addSql('ALTER TABLE file ADD discr VARCHAR(255) NOT NULL, DROP grave_id, DROP graveyard_id, DROP `primary`');
         $this->addSql('ALTER TABLE grave ADD main_image_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\'');
         $this->addSql('ALTER TABLE grave ADD CONSTRAINT FK_21AEDEE7E4873418 FOREIGN KEY (main_image_id) REFERENCES file_grave (id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_21AEDEE7E4873418 ON grave (main_image_id)');
+        $this->addSql('CREATE UNIQUE INDEX unique_entity ON grave (sector, row, number, graveyard_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE grave DROP FOREIGN KEY FK_21AEDEE7E4873418');
-        $this->addSql('ALTER TABLE file_grave DROP FOREIGN KEY FK_8B2D6ED0E439654A');
-        $this->addSql('ALTER TABLE file_grave DROP FOREIGN KEY FK_8B2D6ED0BF396750');
-        $this->addSql('DROP TABLE file_grave');
         $this->addSql('DROP INDEX UNIQ_21AEDEE7E4873418 ON grave');
+        $this->addSql('DROP INDEX unique_entity ON grave');
         $this->addSql('ALTER TABLE grave DROP main_image_id');
         $this->addSql('ALTER TABLE file ADD grave_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', ADD graveyard_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', ADD `primary` TINYINT(1) DEFAULT NULL, DROP discr');
+        $this->addSql('ALTER TABLE file ADD CONSTRAINT FK_8C9F3610E439654A FOREIGN KEY (grave_id) REFERENCES grave (id) ON DELETE SET NULL');
+        $this->addSql('ALTER TABLE file ADD CONSTRAINT FK_8C9F3610B0C855CA FOREIGN KEY (graveyard_id) REFERENCES graveyard (id) ON DELETE SET NULL');
+        $this->addSql('CREATE INDEX IDX_8C9F3610B0C855CA ON file (graveyard_id)');
+        $this->addSql('CREATE INDEX IDX_8C9F3610E439654A ON file (grave_id)');
     }
 }
