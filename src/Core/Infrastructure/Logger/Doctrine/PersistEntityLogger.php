194b6d62b1c732f3bb1b7cd426d0828a0d25d976
Tomasz Kaliński <tomkalon@gmail.com>
2024-01-25 22:44:27 +0100

    logger fix

--- src/Core/Infrastructure/Logger/Doctrine/PersistEntityLogger.php
+++ src/Core/Infrastructure/Logger/Doctrine/PersistEntityLogger.php
@@ -7,7 +7,8 @@ class PersistEntityLogger extends AbstractEntityLogger
     public function createMessage(object $entity): string
     {
         $username = $this->getLoggedUsername();
+        $itemData = $this->getItemData($entity);
         $entityName = $this->getEntityName($entity);
-        return sprintf('%s has been created by %s', $entityName, $username);
+        return sprintf('%s created by %s -> %s', $entityName, $username, $itemData);
     }
 }
